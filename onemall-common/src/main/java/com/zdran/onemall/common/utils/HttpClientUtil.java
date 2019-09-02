6182fb35e05e1c5f16082668291aabd41a5201c8
zdRan <cm.zdran@gmail.com>
2019-09-02 21:57:36 +0800

    添加 http client

---
+++ onemall-common/src/main/java/com/zdran/onemall/common/utils/HttpClientUtil.java
@@ -0,0 +1,21 @@
+package com.zdran.onemall.common.utils;
+
+import java.util.Map;
+
+/**
+ * HTTP client
+ *
+ * Create by ranzd on 2019/9/2
+ *
+ * @author cm.zdran@gmail.com
+ */
+public class HttpClientUtil {
+
+    public static String get(String url, Map<String, Object> param) {
+        return "";
+    }
+
+    public static String post(String url, Map<String, Object> param) {
+        return "";
+    }
+}
